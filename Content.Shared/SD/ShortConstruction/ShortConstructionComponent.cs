ee053edba9f2355ba18e75d691e1b59995bbe912
Farrellka-dev <denfrae54@gmail.com>
2025-04-27 01:20:49 +0300

    ElfUpdate

---
+++ Content.Shared/SD/ShortConstruction/ShortConstructionComponent.cs
@@ -0,0 +1,11 @@
+﻿using Content.Shared.SD.RadialSelector;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SD.ShortConstruction;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShortConstructionComponent : Component
+{
+    [DataField(required: true)]
+    public List<RadialSelectorEntry> Entries = new();
+}
